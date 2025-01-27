20589967fbf5fef7ce10238091add10aaf1eb19e
NakulMital <mitaln@rose-hulman.edu>
2025-01-27 02:00:28 -0500

    Changed the backround of login page to a video.

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,33 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { SignInButton, useSignIn } from '@clerk/clerk-react';
 import './login.css';
+import videoBg from '../Login/Bloomloginbg.mp4';
 
 function Login() {
     const { openSignIn } = useSignIn();
+    const videoRef = useRef(null);
 
     const handleLoginClick = () => {
         openSignIn();
     };
 
-    return (
-        <div id="login">
-            <div id="login-top">
-                <div id="login-logo">Bloom</div>
-                <div id="login-tagline">Better mental health</div>
-            </div>
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.play().catch(error => {
+                console.error("Video playback failed:", error);
+            });
+        }
+    }, []);
 
-            <div id="login-bottom">
-                {/* Customize the button styling */}
-                <SignInButton
-                    mode="modal"
-                >
-                    <button
-                        id="login-buttons-buttons-login"
-                        className="login-button"
-                    >
-                        Continue
-                    </button>
-                </SignInButton>
+    return (
+        <div>
+            <div id='video'>
+                <video
+                    id='login-bg'
+                    ref={videoRef}
+                    src={videoBg}
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    onCanPlay={() => videoRef.current.play()}
+                ></video>
+            </div>
+            <div id="login">
+                <div id="login-top">
+                    <div id="login-logo">Bloom</div>
+                    <div id="login-tagline">Better mental health</div>
+                </div>
+
+                <div id="login-bottom">
+                    <SignInButton mode="modal">
+                        <button
+                            id="login-buttons-buttons-login"
+                            className="login-button"
+                        >
+                            Continue
+                        </button>
+                    </SignInButton>
+                </div>
             </div>
         </div>
     );
